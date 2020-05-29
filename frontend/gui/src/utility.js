2247569931131ddd187246dd6c438aca3f485af2
max <maxdunn123@gmail.com>
2020-05-29 10:31:28 -0700

    improve object manipulation utilities

--- frontend/gui/src/utility.js
+++ frontend/gui/src/utility.js
@@ -1,3 +1,27 @@
-export function updateObject(oldObject, updatedProperties) {
-  return { ...oldObject, ...updatedProperties };
-}
+import cloneDeep from "lodash";
+
+export const updateObjectProperties = (object, properties) => {
+    return { ...cloneDeep(object), ...cloneDeep(properties) };
+};
+export const deleteObjectProperty = (object, property) => {
+    let deepClone = cloneDeep(object);
+    delete deepClone[property];
+    return deepClone;
+};
+export const deleteObjectProperties = (object, properties) => {
+  
+  let deepClone = cloneDeep(object);
+  for (let prop of properties){
+    delete deepClone[prop];
+  }
+  return deepClone;
+};
+export const filterObjectProperties = (object, properties) => {
+    let deepClone = cloneDeep(object);
+    return Object.keys(deepClone)
+        .filter((key) => properties.includes(key))
+        .reduce((obj, key) => {
+            obj[key] = deepClone[key];
+            return obj;
+        }, {});
+};
